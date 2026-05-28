6903bc8ead926748710dfbe368757f7ac19c8c88
joelson <joelsonandradep@gmail.com>
2026-05-28 14:15:20 -0300

    feat: add login screen

---
+++ screens/login.py
@@ -0,0 +1,9 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('kv/login.kv')
+
+
+class LoginScreen(Screen):
+    def fazer_login(self):
+        pass
