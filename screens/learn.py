e1776c374913795a7ba22766b895fb17ba9df555
joelson <joelsonandradep@gmail.com>
2026-05-28 15:11:03 -0300

    feat: add screen modules

---
+++ screens/learn.py
@@ -0,0 +1,11 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+Builder.load_file('kv/learn.kv')
+
+
+class LearnScreen(Screen):
+    pass
+
+class ContentScreen(Screen):
+    pass
